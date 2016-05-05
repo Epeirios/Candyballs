e2ceb5568ea37005cf90e755e3a48209638f3653
Sebas <sebastiaan6@gmail.com>
2016-05-05 17:46:39 +0200

    Adds navbar, login and bootstrap(css)

--- candyapp/app.js
+++ candyapp/app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var login = require('./routes/login');
 
 var app = express();
 
@@ -22,8 +22,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+/**************************************
+* Assigning routes (to urls) :
+*/
 app.use('/', routes);
-app.use('/users', users);
+app.use('/login', login);
+
+
+/**************************************
+* Error handling :
+*/
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -56,5 +65,9 @@ app.use(function(err, req, res, next) {
   });
 });
 
+
+/**************************************
+* Exporting set up:
+*/
 
 module.exports = app;
