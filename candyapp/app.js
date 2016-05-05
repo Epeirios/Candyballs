74fc6d40555c968900ebcc08de0af5187244a990
Epeirios <kevinheugen1996@gmail.com>
2016-05-06 00:58:12 +0200

    added about page

--- candyapp/app.js
+++ candyapp/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var login = require('./routes/login');
+var about = require('./routes/about');
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 */
 app.use('/', routes);
 app.use('/login', login);
+app.use('/about', about);
 
 
 /**************************************
